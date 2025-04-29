47f3b9f41bc5bf0c7871e345aafe1b03d5355742
Developer <santhosh.hegde262000@gmail.com>
2025-04-29 11:25:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const app = express();
+const port = 3000;
+app.use(cors());
+
+app.get("/api", async (req, res) => {
+  const url =
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9400912&lng=77.5300291&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+      },
+    });
+    res.json(response.data);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Failed to fetch data");
+  }
+});
+
+app.listen(port, () => console.log(`Proxy server running on port ${port}`));
